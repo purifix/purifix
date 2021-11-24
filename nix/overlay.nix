7b099b6250634bd18b3ef5cf9973d89bbdc1379a
(cdep)illabout <cdep.illabout@gmail.com>
2021-11-24 14:13:50 +0900

    Add comments.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,7 +1,15 @@
 final: prev: {
 
+  # This is the purescript2nix function.  This makes it easy to build a
+  # PureScript package with Nix.  This is the main function provided by this
+  # repo.
   purescript2nix = final.callPackage ./build-support/purescript2nix {};
 
+  # This is an example PureScript package that has been built by the
+  # purescript2nix function.
+  #
+  # This is just a test that purescript2nix actually works, as well an example
+  # that end users can base their own code off of.
   example-purescript-package = final.purescript2nix {
     pname = "example-purescript-package";
     version = "0.0.1";
