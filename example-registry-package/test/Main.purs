7c41e72f75009853d0ab0262e8172c1fb61964db
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-01-26 02:25:53 +0900

    Make a nicer test for the example

--- example-registry-package/test/Main.purs
+++ example-registry-package/test/Main.purs
@@ -3,9 +3,14 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Aff(launchAff_)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main = launchAff_ $ runSpec [consoleReporter] do
+  describe "true" do
+     it "should be true" do
+        true `shouldEqual` true
