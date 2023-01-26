5296b6107df2a20218a61c1cd16f699447ba83ad
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-01-26 23:19:17 +0900

    disallow binary caches for fast builds
    It's faster to read the file in the already downloaded
    registry-index repo than it is to see if the read file
    is available on the binary cache.
    
    This commit ensures that for such trivial derivations
    no time is spent on accessing the binary cache.

--- nix/build-support/purescript2nix/from-yaml.nix
+++ nix/build-support/purescript2nix/from-yaml.nix
@@ -2,6 +2,8 @@
 # Parse text containing YAML content into a nix expression.
 yaml:
 builtins.fromJSON (builtins.readFile (stdenv.mkDerivation {
+  preferLocalBuild = true;
+  allowSubstitutes = false;
   name = "fromYAML";
   phases = [ "buildPhase" ];
   buildPhase = "echo '${yaml}' | ${yaml2json}/bin/yaml2json > $out";
